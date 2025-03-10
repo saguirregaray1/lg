7aab53635562a96a6dbe04c3a63062428e00a758
saguirregaray1 <saguirregaray1@correo.um.edu.uy>
2025-03-10 15:22:10 -0300

    Update config
    Add cors allow all origins for testing, update commands.yaml

--- app/main.py
+++ app/main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from .commands.router import router as commands_router
 from .devices.router import router as devices_router
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allow all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all methods
+    allow_headers=["*"],  # Allow all headers
+)
 
 app.include_router(devices_router, prefix="/devices")
 app.include_router(commands_router, prefix="/commands")
